c48cc3f2353f8e44ea657796b07b2f03d84b19a0
Kevin Wierman <kwierman@gmail.com>
2017-04-25 14:11:05 -0500

    Adding in the data generator patterns

--- proton_decay_study/generators/base.py
+++ proton_decay_study/generators/base.py
@@ -1,15 +1,15 @@
+import logging
+import abc
 
-class UBooNEDataGenerator(object):
-  logger = logging.getLogger("uboone.data")
-  def __init__(self, datapath, dataset, labelset):
-    self.logger.info("Assembling DataSet")
-    self._file = h5py.File(datapath,'r')
-    self._dataset = self._file[dataset]
-    self._labelset = self._file[labelset]
-    self.current_index=0
+class BaseDataGenerator(object)
+  logger = logging.getLogger('pdk.generator')
+  __metaclass__ = abc.ABCMeta
+
+  def __init__(self):
+    self._dataset = None
 
   def __len__(self):
-    return self._dataset.shape[0]
+    return len(self._dataset)
 
   def __iter__(self):
     return self
@@ -17,16 +17,6 @@ class UBooNEDataGenerator(object):
   def __next__(self):
     return self.next()
 
+  @abstractmethod
   def next(self):
-    batch_size = 10
-    #This next bit causes the generator to loop indefinitely
-    if self.current_index>= len(self):
-        self.logger.info("Reusing Data at Size: {}".format(len(self)))
-        self.current_index = 0
-    if self.current_index+batch_size>= len(self):
-        batch_size = len(self)-current_index
-    x = self._dataset[self.current_index:self.current_index+batch_size]
-    y = self._labelset[self.current_index:self.current_index+batch_size]
-    self.current_index+=batch_size
-
-    return (x,y)
+    raise StopIteration()
