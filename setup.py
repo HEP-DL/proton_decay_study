4b4b6f1ef9a2588e8393bd3206d6d3ec23c48699
Kevin Wierman <kwierman@gmail.com>
2017-04-26 11:39:51 -0500

    Forgot to add the CLI for training

--- setup.py
+++ setup.py
@@ -26,6 +26,7 @@ setup(
     entry_points={
         'console_scripts': [
             'proton_decay_study=proton_decay_study.cli:main'
+            'vgg_training=proton_decay_study.cli:standard_vgg_training'
         ]
     },
     include_package_data=True,
