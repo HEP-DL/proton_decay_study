aaf8ec14730ad0140474ff5251d27bdf15a78ad0
Kevin Wierman <kwierman@gmail.com>
2017-04-26 22:00:26 -0500

    Trying something out

--- proton_decay_study/cli.py
+++ proton_decay_study/cli.py
@@ -19,8 +19,8 @@ def standard_vgg_training(file_list):
 
   generator = MultiFileDataGenerator(file_list, 'image/wires','label/type', batch_size=1)
   model = VGG16(generator)
-  training_output = model.fit_generator(generator, steps_per_epoch = 10000, 
-                                      epochs=200)
+  training_output = model.fit_generator(generator, steps_per_epoch = 10, 
+                                      epochs=2)
   model.save("trained_weights.h5")
   open('history.json','w').write(training_output)
   logger.info("Done.")
