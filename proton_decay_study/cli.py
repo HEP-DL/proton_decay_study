e072737d24262f566ebd43f1c8de7569fc2a3e31
Kevin Wierman <kwierman@gmail.com>
2017-07-18 14:38:22 -0500

    Next attempt at multithreading fix

--- proton_decay_study/cli.py
+++ proton_decay_study/cli.py
@@ -107,9 +107,9 @@ def train_kevnet(steps, epochs, weights, history, output, file_list):
                                      )
   training_output = model.fit_generator(generator, steps_per_epoch=steps,
                                         epochs=epochs,
-                                        workers=2,
+                                        workers=1,
                                         verbose=1,
-                                        max_q_size=1,
+                                        max_q_size=2,
                                         pickle_safe=False,
                                         callbacks=[model_checkpoint])
   model.save(output)
