48de772398d28696ced9aa452cea96e998a4de54
Kevin Wierman <kwierman@gmail.com>
2017-07-20 13:58:10 -0500

    More on memory usage

--- proton_decay_study/models/stages/stage2.py
+++ proton_decay_study/models/stages/stage2.py
@@ -3,7 +3,7 @@ from keras.layers.convolutional import MaxPooling3D, Conv3D
 import logging
 
 class Stage2(Stage1):
-  logger = logging.getLogger('pdk.stage1')
+  logger = logging.getLogger('pdk.stage2')
 
   def assemble_layers(self, layer):
     layer = super(Stage1, self).assemble_layers(layer)
@@ -11,7 +11,7 @@ class Stage2(Stage1):
                    activation='relu', padding='same',
                    data_format='channels_first')(layer)
     self.logger.info(layer)
-    layer = MaxPooling3D((1, 3, 3), strides=(1, 2, 2),
+    layer = MaxPooling3D((1, 2, 2), strides=(1, 2, 2),
                          data_format='channels_first')(layer)
     self.logger.info(layer)
     return layer
