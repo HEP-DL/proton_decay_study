bf6b344e25e18b757d74b7adf43ac350eb5c5a8c
Kevin Wierman <kwierman@gmail.com>
2017-07-20 13:53:54 -0500

    Now using less memory

--- proton_decay_study/models/stages/stage1.py
+++ proton_decay_study/models/stages/stage1.py
@@ -6,11 +6,11 @@ class Stage1(BaseNet):
   logger = logging.getLogger('pdk.stage1')
 
   def assemble_layers(self, layer):
-    layer = Conv3D(64, (1, 5, 5), strides=(1, 5, 5),
+    layer = Conv3D(64, (1, 5, 5), strides=(1, 4, 4),
                        activation='relu', padding='same',
                        data_format='channels_first')(self._input)
     self.logger.info(layer)
-    layer = MaxPooling3D((1, 2, 2), strides=(1, 2, 2),
+    layer = MaxPooling3D((1, 4, 4), strides=(1, 4, 4),
                          data_format='channels_first')(layer)
     self.logger.info(layer)
     return layer
