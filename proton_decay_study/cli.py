afb7c5219817124e30d9598522784bd5e4f3fca3
Kevin Wierman <kwierman@gmail.com>
2017-07-22 13:46:10 -0500

    Minor Changes to tensorboard

--- proton_decay_study/cli.py
+++ proton_decay_study/cli.py
@@ -223,7 +223,7 @@ def train_stagenet(steps, epochs, weights, history, output, stage, file_list):
                                      )
   history_checkpoint = HistoryRecord(history.replace('.json','.csv'))
   es_callback =  EarlyStopping(monitor='loss', min_delta=0, patience=5, verbose=1, mode='auto')
-  tb_callback = TensorBoard()
+  tb_callback = TensorBoard(write_grads=True, write_images=True)
   logging.info("Starting Training [Moving to GPU]")
   training_output = model.fit_generator(generator,
                                         use_multiprocessing=False,
